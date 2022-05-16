35b47d9054dd83aab0e7957aa2ec6767584e0439
Franco Oliva <franco.oliva1991@gmail.com>
2022-05-16 14:30:59 -0300

    Rutas a partir de un path especifico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { AuthRoutingModule } from './auth/auth.routing';
+
 import { Error404Component } from './error404/error404.component';
 import { PagesRoutingModule } from './pages/pages.routing';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  // path: '/dashboard' PagesRouting
+  // path: '/auth'      AuthRouting
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: Error404Component },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes), PagesRoutingModule],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
