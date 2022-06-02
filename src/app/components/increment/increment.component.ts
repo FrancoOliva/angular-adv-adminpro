450adc9dffb6106f3f2efd48f7f708f795fce36c
Franco Oliva <franco.oliva1991@gmail.com>
2022-06-02 12:34:16 -0300

    Implementamos output en componente increment

--- src/app/components/increment/increment.component.ts
+++ src/app/components/increment/increment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-increment',
@@ -6,17 +6,21 @@ import { Component, Input } from '@angular/core';
   styles: [],
 })
 export class IncrementComponent {
-  @Input() progress: number = 10;
+  @Input() progress: number = 40;
+  @Output() value: EventEmitter<number> = new EventEmitter<number>();
 
   changeValue(value: number) {
     if (this.progress >= 100 && value >= 0) {
       this.progress = 100;
+      this.value.emit(100);
       return;
     }
     if (this.progress <= 0 && value < 0) {
       this.progress = 0;
+      this.value.emit(0);
       return;
     }
     this.progress = this.progress + value;
+    this.value.emit(this.progress);
   }
 }
